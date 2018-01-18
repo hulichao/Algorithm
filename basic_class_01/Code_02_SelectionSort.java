bc1e67aa0eabae3e8f73ac422634326924be9756
hulichao <jsu_hlc@163.com>
2018-01-18 12:00:00 +0800

    修改 Code_02_SelectionSort

--- basic_class_01/Code_02_SelectionSort.java
+++ basic_class_01/Code_02_SelectionSort.java
@@ -11,6 +11,15 @@ public class Code_02_SelectionSort {
 		for (int i = 0; i < arr.length - 1; i++) {
 			int minIndex = i;
 			for (int j = i + 1; j < arr.length; j++) {
+
+
+
+
+
+
+
+
+
 				minIndex = arr[j] < arr[minIndex] ? j : minIndex;
 			}
 			swap(arr, i, minIndex);
