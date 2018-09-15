086779039cfae7c298a1e3c42189d36dcf119a64
hulichao <jsu_hlc@163.com>
2018-09-15 13:53:56 +0800

    selectSort update

--- basic_class_01/Code_02_SelectionSort.java
+++ basic_class_01/Code_02_SelectionSort.java
@@ -1,9 +1,87 @@
-// package basic_class_01;
+package basic_class_01;
 
 import java.util.Arrays;
 
 public class Code_02_SelectionSort {
 
+	
+	//bubble
+	public static void bubbleSort2(int[] arr) {
+		if (arr == null || arr.length < 2)
+			return;
+		for(int end = arr.length-1; end > 0; end --){
+			for(int i = 0; i < end;i++){
+				if (arr[i] > arr[i+1])
+					swap(arr, i, i+1);
+			}
+		}
+	}
+	
+	//mergeSort
+	public static void mergeSort2(int[] arr) {
+		if (arr == null || arr.length < 2) 
+			return;
+		process(arr, 0, arr.length - 1);
+	}
+	
+	private static void process(int[] arr, int l, int r) {
+		if (l == r)
+			return;
+		int mid = l + (r-l) >> 1;
+		process(arr, l, mid);
+		process(arr, mid + 1, r);
+		merge(arr, l, mid, r);
+	}
+	
+	private static void merge(int[] arr, int l, int mid, int r) {
+		int[] help = new int[r - l + 1];
+		int i = 0;
+		int p1 = l;
+		int p2 = mid +1;
+		while (p1 <= mid && p2 <= r) {
+			if (arr[p1] < arr[p2])
+				help[i++] = arr[p1++];
+			else
+				help[i++] = arr[p2++];
+		}
+		
+		while (p1 <= mid){
+			help[i++] = arr[p1++];
+		}
+		while (p2 <= mid){
+			help[i++] = arr[p2++];
+		}
+		for (i = 0; i < help.length; i++) {
+			arr[l + i] = help[i];
+		}
+
+	}
+	//select
+	public static void selectSort2(int[] arr){
+		if (arr == null || arr.length < 2)
+			return;
+		for (int i = 0; i < arr.length -1; i++){
+			int minIndex = i;
+			for(int j = i+1; j < arr.length; j++){
+				minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+			}
+			swap(arr, minIndex, i);
+		}
+	}
+	
+	//insertSort
+	public static void insertSort2(int[] arr) {
+		if (arr == null || arr.length < 2)
+			return;
+		for(int i = 1; i < arr.length; i++) {
+			for (int j = i-1; j >= 0 && arr[j+1] < arr[j]; j-- ){
+				swap(arr, j, j+1);
+			}
+		}
+	}
+	
+	
+	
 	public static void selectionSort(int[] arr) {
 		if (arr == null || arr.length < 2) {
 			return;
@@ -11,15 +89,6 @@ public class Code_02_SelectionSort {
 		for (int i = 0; i < arr.length - 1; i++) {
 			int minIndex = i;
 			for (int j = i + 1; j < arr.length; j++) {
-
-
-
-
-
-
-
-
-
 				minIndex = arr[j] < arr[minIndex] ? j : minIndex;
 			}
 			swap(arr, i, minIndex);
@@ -97,7 +166,7 @@ public class Code_02_SelectionSort {
 		for (int i = 0; i < testTime; i++) {
 			int[] arr1 = generateRandomArray(maxSize, maxValue);
 			int[] arr2 = copyArray(arr1);
-			selectionSort(arr1);
+			mergeSort2(arr1);
 			comparator(arr2);
 			if (!isEqual(arr1, arr2)) {
 				succeed = false;
@@ -110,7 +179,7 @@ public class Code_02_SelectionSort {
 
 		int[] arr = generateRandomArray(maxSize, maxValue);
 		printArray(arr);
-		selectionSort(arr);
+		mergeSort2(arr);
 		printArray(arr);
 	}
 
